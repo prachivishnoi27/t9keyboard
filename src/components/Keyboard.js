581ed7515c5a0461007d4de67f3fd3f8f2aba312
Prachi Vishnoi <prachi27vishnoi@gmail.com>
2021-05-27 16:56:20 +0530

    capitals working

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Keyboard.css";
 import  { setText } from "./KeyboardUtil";
 
 const Keyboard = ({ message, setMessage }) => {
+    const [isCap, setIsCap] = useState(0);
     const handleClick = (e) => {
         const id = e.target.id;
-        setMessage(setText(Number(id), message));
+        let result = setText(Number(id), message, isCap);
+        setMessage(result.message);
+        setIsCap(result.isCap);
     }
 
   return (
@@ -17,21 +20,21 @@ const Keyboard = ({ message, setMessage }) => {
       </div>
       <div className="row">
         <button id="4" onClick={handleClick} className="key">.?!</button>
-        <button id="5" onClick={handleClick} className="key">abc</button>
-        <button id="6" onClick={handleClick} className="key">def</button>
+        <button id="5" onClick={handleClick} className="key">{isCap === 0?'abc': (isCap === 1? "Abc": "ABC")}</button>
+        <button id="6" onClick={handleClick} className="key">{isCap === 0?'def': (isCap === 1? "Def": "DEF")}</button>
       </div>
       <div className="row">
-        <button id="7" onClick={handleClick} className="key">ghi</button>
-        <button id="8" onClick={handleClick} className="key">jki</button>
-        <button id="9" onClick={handleClick} className="key">mno</button>
+        <button id="7" onClick={handleClick} className="key">{isCap === 0?'ghi': (isCap === 1? "Ghi": "GHI")}</button>
+        <button id="8" onClick={handleClick} className="key">{isCap === 0?'jkl': (isCap === 1? "Jkl": "JKL")}</button>
+        <button id="9" onClick={handleClick} className="key">{isCap === 0? 'mno': (isCap === 1? "Mno": "MNO")}</button>
       </div>
       <div className="row">
-        <button id="10" onClick={handleClick} className="key">pqrs</button>
-        <button id="11" onClick={handleClick} className="key">tuv</button>
-        <button id="12" onClick={handleClick} className="key">wxyz</button>
+        <button id="10" onClick={handleClick} className="key">{isCap === 0?'pqrs': (isCap === 1? "Pqrs": "PQRS")}</button>
+        <button id="11" onClick={handleClick} className="key">{isCap === 0?'tuv': (isCap === 1? "Tuv": "TUV")}</button>
+        <button id="12" onClick={handleClick} className="key">{isCap === 0?'wxyz': (isCap === 1? "Wxyz": "WXYZ")}</button>
       </div>
       <div className="row">
-        <button id="13" onClick={handleClick} className="key">1*a</button>
+        <button id="13" onClick={handleClick} className="key">*#$@</button>
         <button id="14" onClick={handleClick} className="key">_</button>
         <button id="15" onClick={handleClick} className="key">&#x2191;</button>
       </div>
--- src/components/KeyboardUtil.js
+++ src/components/KeyboardUtil.js
@@ -1,50 +1,73 @@
 let last_press = -1;
 let press_time = new Array(15);
+let cap1_cnt = 0;
 
 let _characters = [
   [".", "?", "!", "1"],
   ["a", "b", "c", "2"],
   ["d", "e", "f", "3"],
   ["g", "h", "i", "4"],
-  ["j", "k", "i", "5"],
+  ["j", "k", "l", "5"],
   ["m", "n", "o", "6"],
   ["p", "q", "r", "s", "7"],
   ["t", "u", "v", "8"],
   ["w", "x", "y", "z", "9"],
+  ["*","#","$","@"]
 ];
 
-export const setText = (id, message) => {
+export const setText = (id, message, isCap) => {
     const d = new Date();
     let cur_time = d.getTime();
+
   if (id === 2) {
     message += "\n";
   }
+
   if (id === 3) {
-    // if(message[message.length-1] === "\n")
+      if(isCap === 1) cap1_cnt--;
     message = message.slice(0, -1);
   }
-  if(id >= 4 && id <= 12) {
+
+  if(id >= 4 && id <= 13) {
       if(last_press === id &&  cur_time - press_time[id] <= 500) {
         let last = message[message.length-1];
         let i = 0, n = _characters[id-4].length;
         while(i < n) {
-            if(last === _characters[id-4][i]) break;
+            if(last.toLowerCase() === _characters[id-4][i]) break;
             i++;
         }
+        // remove previous character
         message = message.slice(0,-1);
-        if(i >= n-2) message += _characters[id-4][n-1];  
-        else message += _characters[id-4][i+1];
+    
+        if(((isCap === 2) || (isCap === 1 && cap1_cnt === 1)) && id !== 13) {
+            if(i >= n-2) message += _characters[id-4][n-1].toUpperCase();  
+            else message += _characters[id-4][i+1].toUpperCase();
+        } else {
+            if(i >= n-2) message += _characters[id-4][n-1];  
+            else message += _characters[id-4][i+1];
+        }
       } else {
-        message += _characters[id-4][0];
+          if((isCap === 2 || (isCap ===1 && cap1_cnt === 0) )&& id !== 13) message += _characters[id-4][0].toUpperCase()
+        else message += _characters[id-4][0];
+        cap1_cnt++;
       } 
   }
+
   if (id === 14) {
     if(last_press === id &&  cur_time - press_time[id] <= 500) {
         message = message.slice(0,-1);  
         message += "0";
     }else message += " ";
   }
+
+  if(id === 15) {
+      isCap = (isCap+1)%3;
+      if(isCap === 1) cap1_cnt = 0;
+  }
+
   last_press = id;
   press_time[id] = cur_time;
-  return message;
+
+
+  return { message, isCap };
 };
